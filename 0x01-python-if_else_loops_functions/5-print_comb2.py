12264740a7bf6d34939db6287de5e77bb4e02e9c
Aliyu Habibu <aliyuhabibu111@gmail.com>
2024-03-25 10:37:33 +0100

    Handle statements

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for x in range(0, 10):
-        print(f"{i}{x}", end='')
+        print("{}{}".format(i, x), end='')
         if not ((i == 9) and (x == 9)):
             print(", ", end='')
 print()
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,9 @@
 for i in range(0, 9):
     for x in range(1, 10):
         if x > i:
-            print(f"{i}{x}, ", end='')
+            print("{}{}".format(i, x), end='')
+            if not ((i == 8) and (x == 9)):
+                    print(", ",end='')
         else:
             continue
+print()
