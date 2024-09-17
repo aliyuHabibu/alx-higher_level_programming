fcbf78c315332a45e9c6553e1132809f72ee274d
Aliyu Habibu <aliyuhabibu111@gmail.com>
2024-09-17 06:19:08 +0100

    Query using ORM

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,15 @@
+#!/home/habeeb/venv/bin/python3
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine(f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}", pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for no, name in session.query(State.id, State.name).order_by(State.id):
+        print(f"{no}: {name}")
