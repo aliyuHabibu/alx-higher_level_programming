14b166e6f0946344153f42d28eca9860b4fe5d07
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-08-16 16:50:04 +0100

    Write a functon that check for lowercase character
    	Prototype: def islower(c):
    	Returns True if c is lowercase
    	Returns False otherwise
    	You are not allowed to import any module
    	You are not allowed to use str.upper() and str.isupper()
    	Tips: ord()
    	You don’t need to understand __import_

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    if (c >= 'a') and (c <= 'z'):
-        return True
-    else:
-        return False
+    if ord(c) in range(97, 122):
+        if (c >= 'a') and (c <= 'z'):
+            return True
+        else:
+            return False
