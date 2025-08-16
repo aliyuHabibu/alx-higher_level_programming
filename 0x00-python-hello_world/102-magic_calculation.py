fa290e8631f7fb1cc1febeb34cb17724a3973289
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-08-16 15:31:57 +0100

    Adjusted a line.

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
-from test import 
 def magic_calculation(a, b):
     """
         Function to practice python bytecode
     """
     return 98 + a ** b
 
-import dis
-print(dis.dis(magic_calculation))
+#import dis
+#print(dis.dis(magic_calculation))
