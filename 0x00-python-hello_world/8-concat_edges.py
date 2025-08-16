b9b09fbb38748774f656c66d615bf4a2bf07469f
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-08-16 12:51:32 +0100

    Adjusted the code

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-first = str[39:-62]
-second, third = str[107:111], str[0:6]
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+first, second, third = str[39:-62], str[107:111], str[0:6]
 print(f"{first}{second} {third}")
