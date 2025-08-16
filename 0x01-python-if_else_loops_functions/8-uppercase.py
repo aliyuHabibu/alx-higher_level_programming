d029399f7ee1f7830bed6c1a50fc9009a6e4e851
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-08-16 17:15:59 +0100

    Updated the code

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        #print(i)
         print("{}".format(i) if ord(i) < 90 else "{:c}".format(ord(i)-32), end='')
-    print()
+    print("{}".format(''))
