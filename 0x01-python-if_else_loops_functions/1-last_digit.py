d1d10b03b437a994f408873afc3c9b691c78946d
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-08-16 16:39:43 +0100

    Check a number:
    	This program will assign a random signed number to the variable number each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable number.
    
    	You can find the source code here
    	The variable number will store a different value every time you will run this program
    	You don’t have to understand what import, random.randint do. Please do not touch this code. This line should not change: number = random.randint(-10000, 10000)
    	The output of the program should be:
    	The string Last digit of, followed by
    	the number, followed by
    	the string is, followed by the last digit of number, followed by
    	if the last digit is greater than 5: the string and is greater than 5
    	if the last digit is 0: the string and is 0
    	if the last digit is less than 6 and not 0: the string and is less than 6 and not 0
    	followed by a new line

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-i = number % 10
-if i > 5:
-    print(f"Last digit of {number} is {i} and is greater than 5")
-elif i == 0:
-    print(F"Last digit of {number} is {i} and is 0")
-elif i < 0 or i > 0 and i < 6:
-    print(f"Last digit of {number} is {i} and is less than 6 and not 0")
+if number > 0:
+    i = number % 10
+    if i == 0:
+        print(F"Last digit of {number} is {i} and is 0")
+    elif i > 5:
+        print(f"Last digit of {number} is {i} and is greater than 5")
+    elif i < 6:
+        print(f"Last digit of {number} is {i} and is less than 6 and not 0")
+else:
+    i = abs(number) % 10
+    if i == 0:
+        print(F"Last digit of {number} is {i} and is 0")
+    else:
+        print(f"Last digit of {number} is -{i} and is less than 6 and not 0")
