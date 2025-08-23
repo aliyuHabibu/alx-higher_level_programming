39a0200e3ffbe23ac8f60697a83a393fc6c5e97b
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-08-23 23:47:02 +0100

    Write a function that print all integers of a list
    	Prototype: def print_list_integer(my_list=[]):
    	Format: one integer per line. See example
    	You are not allowed to import any module
    	You can assume that the list only contains integers
    	You are not allowed to cast integers into strings
    	You have to use str.format() to print integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+"""
+    Function to print the element
+    of a list
+"""
+
+
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
         print("{}".format(my_list[i]))
