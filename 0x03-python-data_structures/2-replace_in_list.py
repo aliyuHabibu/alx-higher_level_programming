1b0bf057153d461e390a6b0e579db5c9c0613098
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-08-23 23:55:20 +0100

    Write a function that replaces an element of a list at a specific position (like in C).
    	Prototype: def replace_in_list(my_list, idx, element):
    	If idx is negative, the function should not modify anything, and returns the original list
    	If idx is out of range (> of number of element in my_list), the function should not modify anything, and returns the original list
    	You are not allowed to import any module
    	You are not allowed to use try/except

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python3
+
+"""
+    Function that replace the element of
+    a list at a given index with another value
+"""
+
+
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
