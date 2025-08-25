9002707550dc0f0af5e5709acdd37151dbac6770
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-08-25 18:13:19 +0100

    Write a function that deletes the item at a specific position in a list.
    	Prototype: def delete_at(my_list=[], idx=0):
    	If idx is negative or out of range, nothing change (returns the same list)
    	You are not allowed to use pop()
    	You are not allowed to import any module

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python3
+
+"""
+    Function to delete an element
+    at a given index.
+"""
+
+
 def delete_at(my_list=[], idx=0):
     if ((idx < 0) or (idx > len(my_list))):
         return my_list
