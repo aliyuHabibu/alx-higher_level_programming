e8622face63b5487c8dad820d2169a6bd4c2f394
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-08-27 20:55:30 +0100

    Write a function that replaces or adds key/value in a dictionary.
    	Prototype: def update_dictionary(a_dictionary, key, value):
    	key argument will be always a string
    	value argument will be any type
    	If a key exists in the dictionary, the value will be replaced
    	If a key doesn’t exist in the dictionary, it will be created
    	You are not allowed to import any module

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+    function to update a value or both value
+    and key pass to the function
+"""
+
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
