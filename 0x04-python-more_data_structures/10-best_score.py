8c775896695d8039bd59a63677811ca1d0406e33
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-08-27 22:53:42 +0100

    Make it meet standard

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,8 +5,9 @@
     with the largest value
 """
 
+
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     else:
         vals = list(a_dictionary.values())
@@ -16,5 +17,3 @@ def best_score(a_dictionary):
                 larVal = a_dictionary[keys]
                 key = keys
         return key
-
-
