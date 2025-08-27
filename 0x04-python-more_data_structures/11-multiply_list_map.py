867a3c30168d35f0e5f1f32d0b0ff23b5a603950
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-08-27 23:09:52 +0100

    Obey Instructions

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-
-"""
-    Function to take each ekement of a list
-    a mutiply it with a given value using map function
-"""
-
-
 def multiply_list_map(my_list=[], number=0):
-    mp = map(lambda x: x * number, my_list)
-    return list(mp)
+    return list(map(lambda x: x * number, my_list))
