dbbee19b837b5990365a6aeec183d031aecebb4e
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-08-29 15:49:05 +0100

    Obey Instructions

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -29,3 +29,45 @@ int is_palindrome(listint_t **head)
 	else
 		return (0);
 }
+
+/**
+ * count_list - function to count nodes
+ * @head: pointer to the head of the list
+ * Return: Number counted.
+ */
+int count_list(listint_t *head)
+{
+        listint_t *current;
+        int n;
+
+        n = 0;
+        current = head;
+        while (current != NULL)
+        {
+                n++;
+                current = current->next;
+        }
+        return (n);
+}
+
+/**
+ * address - Function to get and return the address of a node
+ * @head: Double pointer to the head address
+ * @total: Total number of linked list
+ * @getThere: Where to stop
+ * Return: Address of the node
+ */
+listint_t *address(listint_t **head, int total, int getThere)
+{
+        listint_t *get;
+        int s;
+
+        get = *head;
+        s = 0;
+        while (s < (total - getThere))
+        {
+                get = get->next;
+                s++;
+        }
+        return (get);
+}
