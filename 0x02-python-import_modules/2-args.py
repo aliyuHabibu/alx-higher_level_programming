fd13da1ab7b68f1f5ba8f7124e5bb6e8f39852e2
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-09-02 17:30:59 +0100

    Write a program that prints the number of and the list of its arguments.
    	The output should be:
    		Number of argument(s) followed by argument (if number is one) or arguments (otherwise), followed by
    			: (or . if no arguments were passed) followed by
    			a new line, followed by (if at least one argument),
    		one line per argument:
    			the position of the argument (starting at 1) followed by :, followed by the argument value and a new line
    	Your code should not be executed when imported
    	The number of elements of argv can be retrieved by using: len(argv)
    	You do not have to fully understand lists yet, but imagine that argv can be used just like a C array: you can use an index to walk through it. There are other ways (which will be preferred for future project tasks), if you know them you can use them.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
-import sys
-args = (len(sys.argv) - 1)
-if args == 0:
-    print(f"{args} arguments.")
-else:
-    print(f"{args} argument:")
-    i = 1
-    while i <= args:
-        print(f"{i}: {sys.argv[i]}")
-        i += 1
 
-__name__ == "__main__"
+"""
+    Script to count and read arguments from
+    command line and print to the stdout
+"""
+
+import sys
+
+args = (len(sys.argv) - 1)
+
+if __name__ == "__main__":
+    if args == 0:
+        print(f"{args} arguments.")
+    elif args == 1:
+        print(f"{args} argument:")
+        print(f"{args}: {sys.argv[args]}")
+    else:
+        print(f"{args} arguments:")
+        i = 1
+        while i <= args:
+            print(f"{i}: {sys.argv[i]}")
+            i += 1
