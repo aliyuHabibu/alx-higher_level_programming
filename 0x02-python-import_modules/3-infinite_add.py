1877fd393cb31f68f118eb825950a7017b94d184
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-09-02 17:40:25 +0100

    Write a program that prints the result of the addition of all arguments.
    	The output should be the result of the addition of all arguments, followed by a new line
    	You can cast arguments into integers by using int() (you can assume that all arguments can be casted into integers)
    	Your code should not be executed when imported

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
+
+"""
+    Script to sum all numbers privided as argument
+    from the standard input.
+"""
+
 from sys import argv
+
 args = (len(argv) - 1)
 ans = 0
-if args == 0:
-    print(ans)
-else:
-    i = 1
-    while i <= args:
-        ans += int(argv[i])
-        i += 1
-    print(ans)
+
+if __name__ == "__main__":
+    if args == 0:
+        print(ans)
+    else:
+        i = 1
+        while i <= args:
+            ans += int(argv[i])
+            i += 1
+        print(ans)
