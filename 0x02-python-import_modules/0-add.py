b8201ee954474925553a90b423dcb7885d7e5222
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-09-02 17:12:51 +0100

    Write a program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3
    	You have to use print function with string format to display integers
    	You have to assign:
    	the value 1 to a variable called a
    	the value 2 to a variable called b
    	and use those two variables as arguments when calling the functions add and print
    	a and b must be defined in 2 different lines: a = 1 and another b = 2
    	Your program should print: <a value> + <b value> = <add(a, b) value> followed with a new line
    	You can only use the word add_0 once in your code
    	You are not allowed to use * for importing or __import__
    	Your code should not be executed when imported - by using __import__, like the example below

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+
+"""
+    Script to import a function and perform
+    an operations with it.
+"""
+
 from add_0 import add
-a = 1
-b = 2
-my_sum = add(a, b)
-print("{} + {} = {}".format(a, b, my_sum))
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    my_sum = add(a, b)
+    print("{} + {} = {}".format(a, b, my_sum))
