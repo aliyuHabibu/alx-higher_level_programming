2edfb03308948bc0c9285008d997580c95799120
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-09-02 21:23:22 +0100

    Write a function that divides element by element 2 lists.
    	Prototype: def list_division(my_list_1, my_list_2, list_length):
    	my_list_1 and my_list_2 can contain any type (integer, string, etc.)
    	list_length can be bigger than the length of both lists
    	Returns a new list (length = list_length) with all divisions
    	If 2 elements can’t be divided, the division result should be equal to 0
    	If an element is not an integer or float:
    		print: wrong type
    	If the division can’t be done (/0):
    		print: division by 0
    	If my_list_1 or my_list_2 is too short
    		print: out of range
    	You have to use try: / except: / finally:
    	You are not allowed to import any module

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+def list_division(my_list_1, my_list_2, list_length):
+    list_ans = []
+    i, ans = 0, 0
+    while i < list_length:
+        try:
+            ans = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError as ze:
+            print("division by 0")
+        except TypeError as ty:
+            print("wrong type")
+        except IndexError as ix:
+            print("out of range")
+        finally:
+            if ans == 0:
+                list_ans.append(ans)
+            else:
+                list_ans.append(ans)
+                ans = 0
+        i += 1
+    return list_ans
