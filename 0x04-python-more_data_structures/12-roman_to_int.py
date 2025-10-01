8d3ca5b9700dbb8e016600f9c9b63e1eb983209f
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-10-01 10:31:31 +0100

    Technical interview preparation:
    	You are not allowed to google anything
    	Whiteboard first
    	Create a function def roman_to_int(roman_string): that converts a Roman numeral to an integer.
    
    		You can assume the number will be between 1 to 3999.
    		def roman_to_int(roman_string) must return an integer
    		If the roman_string is not a string or None, return 0

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+    Module to get the number of integer equivalent
+    of a given roman symbols of numeral
+"""
+
+
+def roman_to_int(roman_string):
+    symb = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    if type(roman_string) == str:
+        rslt = 0
+        i = 0
+        while (i < len(roman_string)):
+            if (i + 1) < (len(roman_string)):
+                if symb[roman_string[i + 1]] > symb[roman_string[i]]:
+                    rslt += symb[roman_string[i + 1]] - symb[roman_string[i]]
+                    i = i + 1
+                else:
+                    rslt += symb[roman_string[i]]
+            else:
+                rslt += symb[roman_string[i]]
+            i = i + 1
+        return rslt
+    else:
+        return 0
