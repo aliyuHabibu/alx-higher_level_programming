ba8445e83e5621855ee2409b2bf17dd31acd9d44
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-10-06 15:07:55 +0100

    Obey rules

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -7,6 +7,8 @@
 
 
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return
     my_list.reverse()
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
