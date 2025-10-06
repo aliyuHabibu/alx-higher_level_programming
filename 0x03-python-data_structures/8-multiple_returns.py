a667e458099575386b871e21244a61eb880743ce
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-10-06 15:30:18 +0100

    Adjust code

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -10,7 +10,8 @@
 def multiple_returns(sentence):
     n = 0
     if len(sentence) == 0:
-        sentence[0] = None
-    for i in range(len(sentence)):
-        n += 1
-    return (n, sentence[0])
+        return (n, None)
+    else:
+        for i in range(len(sentence)):
+            n += 1
+        return (n, sentence[0])
