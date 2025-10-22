99f2e83ba361fa5e249faee814692c85f9be2f4b
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-10-22 16:23:51 +0100

    Define a class retangle:
    	Definition of the class rectangle with
    	getter and setter methods.

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+    Module that define the class rectangle
+"""
+
+
+class Rectangle:
+    """
+        Definition of the class rectangle
+    """
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        else:
+            self.__width = value
