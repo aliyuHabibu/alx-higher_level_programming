d278d216630b5c6aaec7ea705b78b51693717812
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-10-22 16:30:34 +0100

    Follow rules

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,15 +10,18 @@ class Rectangle:
         Definition of the class rectangle
     """
     def __init__(self, width=0, height=0):
+        "Initialization method for all object variables"
         self.height = height
         self.width = width
 
     @property
     def height(self):
+        "The getter method for the height instance variable"
         return self.__height
 
     @height.setter
     def height(self, value):
+        "The setter method for the height instance variable"
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
 
@@ -30,10 +33,12 @@ class Rectangle:
 
     @property
     def width(self):
+        "Getter method for width instance variable"
         return self.__width
 
     @width.setter
     def width(self, value):
+        "Stter method for width instance variable"
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
 
