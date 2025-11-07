78fb61e69931be0f0d7ce4e5cc234af204875337
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-11-07 06:21:27 +0100

    Write a class Student that defines a student by: (based on 10-student.py):
    	Public instance attributes:
    		first_name
    		last_name
    		age
    	Instantiation with first_name, last_name and age: def __init__(self, first_name, last_name, age):
    	Public method def to_json(self, attrs=None): that retrieves a dictionary representation of a Student instance (same as 8-class_to_json.py):
    		If attrs is a list of strings, only attributes name contain in this list must be retrieved.
    		Otherwise, all attributes must be retrieved
    	Public method def reload_from_json(self, json): that replaces all attributes of the Student instance:
    		You can assume json will always be a dictionary
    		A dictionary key will be the public attribute name
    		A dictionary value will be the value of the public attribute
    	You are not allowed to import any module
    	Now, you have a simple implementation of a serialization and deserialization mechanism (concept of representation of an object to another format, without losing any information and allow us to rebuild an object based on this representation)

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+    Module that define a class with an intiliazation
+    method and a method to return the dict of instances
+"""
+
+
+class Student:
+    "Student class definition"
+    def __init__(self, first_name, last_name, age):
+        "Initialization method for all class instances"
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        "Method a return a dict rep of class instances"
+        if attrs is None:
+            return self.__dict__
+        else:
+            my_dict = {}
+            for attr in attrs:
+                try:
+                    my_dict[attr] = getattr(self, attr)
+                except AttributeError:
+                    continue
+            return my_dict
+
+    def reload_from_json(self, json):
+        "
+        Method to update instances attribute
+        with given attribute in given dict
+        "
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
